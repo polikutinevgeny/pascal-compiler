2032b64c2ae176df9bf04933399c01172a8288e7
polikutinevgeny <polikutinevgeny@gmail.com>
2017-10-06 11:58:44 +1000

    Add project files.

---
+++ pascal-compiler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pascal_compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
